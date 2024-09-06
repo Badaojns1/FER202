d35b268f2f789594f2bab610dd1ce9ab558b517b
Badaojns1 <thangbadao50@gmail.com>
2024-09-06 10:19:15 +0700

    Second commit

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React ha ha
         </a>
       </header>
     </div>
